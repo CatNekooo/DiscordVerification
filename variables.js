7a8f8e1a7c2308763cc551e9b76b063e90b7f401
CatNekooo <76517522+CatNekooo@users.noreply.github.com>
2020-12-27 20:59:39 +0900

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,10 +1,10 @@
 // Discord info
-const discord_bot_token = '';
-const discord_guild_id = '';
-const discord_verified_role = '';
+const discord_bot_token = 'Nzc4OTM4MjcxOTU4MDQwNjM2.X7ZQzA.vvA2WimBWEPIIHRAxvwcmbpJ2MY';
+const discord_guild_id = '778609767735885856';
+const discord_verified_role = '778931319421272065';
 
 // reCAPTCHA v2 secret key
-const recaptcha_secret_key = '';
+const recaptcha_secret_key = '6Lf1lxYaAAAAALKKRbi7a5bEOq71cSa8T_DF5a5q';
 
 // Domain
 const domain = ''; // Leave empty for localhost
